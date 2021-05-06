5fa030d6594113c9c611cdb60bbd4b6a285954a4
Bafana <76040794+Bafana29@users.noreply.github.com>
2021-05-06 13:30:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,8 @@ function TakeSnapshot(){
 
 console.log("ml5 version ", ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/COfk2MBRs/model.json', modelLoaded);
+
+function check(){
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
